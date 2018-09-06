9e799e01768b32f900847a52fc90eb3ef5259868
alanna.botscharow <alanna.botscharow@CLC-LAB209-18.TCSD.tooelesd.org>
2018-09-06 14:12:00 -0600

    Chapter 2 Program 1

---
+++ src/AlannaChapter2Program1/Chapter2Program1.java
@@ -0,0 +1,17 @@
+package AlannaChapter2Program1;
+/*
+ Alanna Botscharow
+ 9/18
+ Chapter2Program1
+ */
+
+public class Chapter2Program1 {
+
+	public static void main(String[] args) {
+		// Displays three different messages.
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to computer science");
+		System.out.println("Programming is fun");
+	}
+
+}
